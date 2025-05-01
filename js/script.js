27ab9a6678b3a42d475ed67f8b6755b29f816d75
Edvaldojs22 <jonas.edvaldo.10@gmail.com>
2025-05-01 16:40:57 -0300

    feat: Creating the Item Search with the Table Buttons

--- js/script.js
+++ js/script.js
@@ -1,46 +1,73 @@
-const dataItems = [];
-const dataFeedbacks = [];
+let dataItems = [];
 
-async function searchData(params) {
+async function searchData() {
   try {
     const response = await fetch("../data/all-items.json");
     if (!response.ok) throw new Error("Error loading JSON");
     const jsonData = await response.json();
-    updateTable(jsonData.data);
+    dataItems = jsonData.data;
+    updateTable(dataItems);
   } catch (error) {
     console.log("Erro:", error);
   }
 }
+searchData();
 
 function updateTable(items) {
-  console.log(items.length)
   const tableBody = document.querySelector("#tableBody");
-  
-  items.forEach((items) => {
-    let price = convertToReal(items.price)
+  tableBody.innerHTML = "";
+
+  items.forEach((item) => {
+    let price = convertToReal(item.price);
     const tr = document.createElement("tr");
+    tr.classList.add("trTbody");
+
     const itemName = document.createElement("td");
-    itemName.textContent = items.name;
+    itemName.classList.add("nameTbody");
+    itemName.textContent = item.name;
 
     const itemType = document.createElement("td");
-    itemType.textContent = items.type;
+    itemType.classList.add("typeTbody");
+    itemType.textContent = item.type;
 
-    const itemPreice = document.createElement("td");
-    itemPreice.textContent = `R$ ${price.toFixed(2)}`;
+    const itemPrice = document.createElement("td");
+    itemPrice.classList.add("priceTbody");
+    itemPrice.textContent = `R$ ${price.toFixed(2)}`;
 
     tr.appendChild(itemName);
     tr.appendChild(itemType);
-    tr.appendChild(itemPreice);
+    tr.appendChild(itemPrice);
 
     tableBody.appendChild(tr);
   });
 }
 
-searchData();
-
-
-function  convertToReal(value) {
-  return   5.66 * value
+function convertToReal(value) {
+  return 5.66 * value;
 }
 
-// 5,66
+const buttonFilter = document.querySelectorAll(".buttonFilter");
+buttonFilter[0].style.backgroundColor = "#F9C06A";
+buttonFilter[0].style.color = "#aa8558";
+
+buttonFilter.forEach((filterButton) =>
+  filterButton.addEventListener("click", (event) => {
+    buttonFilter.forEach((button) => {
+      button.style.backgroundColor = "";
+      button.style.color = "#aa8558";
+    });
+
+    const typeToFilter = filterButton.getAttribute("data-type");
+
+    event.target.style.backgroundColor = "#F9C06A";
+    event.target.style.color = "#000000";
+    if (typeToFilter === "All") {
+      updateTable(dataItems);
+    } else {
+      const filteredItems = dataItems.filter(
+        (item) => item.type === typeToFilter
+      );
+      updateTable(filteredItems);
+    }
+  })
+);
