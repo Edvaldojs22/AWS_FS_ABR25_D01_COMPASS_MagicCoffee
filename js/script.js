5dcf0d8df524e273f0da9868be4af4375d76f223
Edvaldojs22 <jonas.edvaldo.10@gmail.com>
2025-05-04 10:35:37 -0300

    feat: Creating Email Validation with Animations

--- js/script.js
+++ js/script.js
@@ -16,13 +16,12 @@ async function searchData() {
 
     dataItems = jsonDataItems.data;
     dataFeedbacks = jsonDataFeedbacks.data;
-
+    dataItmesNew(dataItems);
     updateTable(dataItems);
     pushFeedbaks();
     $("#carrosel").slick({
       arrows: true,
-      autoplay: true,
-      autoplaySpeed: 3000,
+
       prevArrow:
         '<button type="button" class="slick-prev"> <img class="imgArrawLeft"  src="assets/img/left-arrow.png" alt=""></button>',
       nextArrow:
@@ -34,6 +33,42 @@ async function searchData() {
 }
 
 searchData();
+
+function dataItmesNew(items) {
+  const dashboardCards = document.querySelector(".dashboardCards");
+  items.forEach((item) => {
+    if (item.is_new) {
+      const card = document.createElement("div");
+      card.classList.add("card");
+      const img = document.createElement("img");
+      img.src = item.imagem_url;
+
+      const cardAtributes = document.createElement("div");
+      cardAtributes.classList.add("cardAtributes");
+      const textName = document.createElement("p");
+      textName.textContent = item.name;
+      const textPercentage = document.createElement("p");
+      textPercentage.textContent = `${item.ingredients_ratio[0].ingredient} ${item.ingredients_ratio[0].percentage}% | ${item.ingredients_ratio[1].ingredient} ${item.ingredients_ratio[1].percentage}%`;
+
+      const textValue = document.createElement("p");
+
+      textValue.textContent = `R$ ${convertToReal(item.price).toFixed(2)}`;
+      const button = document.createElement("button");
+      button.textContent = "Order now";
+      button.classList.add("buttonOrder");
+
+      cardAtributes.appendChild(textName);
+      cardAtributes.appendChild(textPercentage);
+      cardAtributes.appendChild(textValue);
+      cardAtributes.appendChild(button);
+
+      card.appendChild(img);
+      card.appendChild(cardAtributes);
+
+      dashboardCards.appendChild(card);
+    }
+  });
+}
 
 function updateTable(items) {
   const tableBody = document.querySelector("#tableBody");
@@ -74,7 +109,6 @@ buttonFilter[0].style.color = "#aa8558";
 
 buttonFilter.forEach((filterButton) =>
   filterButton.addEventListener("click", (event) => {
-    console.log(dataFeedbacks);
     buttonFilter.forEach((button) => {
       button.style.backgroundColor = "";
       button.style.color = "#aa8558";
@@ -125,4 +159,97 @@ function pushFeedbaks() {
 
     painelFeedbak.appendChild(painel);
   });
+}
+
+let saved;
+const textApproved = document.querySelector(".textApproved");
+const iconLoading = document.querySelector(".iconLoading");
+const buttonEmail = document.querySelector("#buttonEmail");
+const inputEmail = document.querySelector("#email");
+let emailValue;
+
+window.addEventListener("DOMContentLoaded", () => {
+  saved = localStorage.getItem("savedEmail");
+  inputEmail.value = saved;
+  if (inputEmail.value !== "") {
+    textApproved.style.display = "block";
+    buttonEmail.style.backgroundColor = "#ff0000";
+    buttonEmail.style.color = "black";
+    buttonEmail.textContent = "Unsubscribe";
+  } else {
+    textApproved.style.display = "none";
+    buttonEmail.style.backgroundColor = "#F9C06A";
+    buttonEmail.textContent = "Subscribe";
+  }
+});
+
+inputEmail.addEventListener("input", () => {
+  emailValue = inputEmail.value;
+  if (saved == emailValue) {
+    buttonEmail.style.backgroundColor = "#ff0000";
+    textApproved.style.display = "block";
+    buttonEmail.textContent = "Unsubscribe";
+  } else {
+    buttonEmail.style.backgroundColor = "#F9C06A";
+    textApproved.style.display = "none";
+    buttonEmail.textContent = "Subscribe";
+  }
+});
+
+function validateEmail(event) {
+  const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+  if (regex.test(inputEmail.value)) {
+    localStorage.setItem("savedEmail", inputEmail.value);
+    saved = inputEmail.value;
+
+    if (inputEmail.value == saved && buttonEmail.textContent == "Unsubscribe") {
+      iconLoading.style.display = "block";
+      buttonEmail.textContent = "";
+      textApproved.style.display = "none";
+
+      setTimeout(() => {
+        localStorage.clear();
+        saved = `"`;
+        iconLoading.style.display = "none";
+        buttonEmail.textContent = "Subscribe";
+        email.value = "";
+        textApproved.style.display = "block";
+        textApproved.style.color = "#ffffff";
+        textApproved.textContent = "Email removed.";
+        buttonEmail.style.backgroundColor = "#F9C06A";
+      }, 2000);
+
+      setTimeout(() => {
+        textApproved.style.display = "none";
+      }, 4500);
+    } else {
+      iconLoading.style.display = "block";
+      buttonEmail.textContent = "";
+      setTimeout(() => {
+        iconLoading.style.display = "none";
+        buttonEmail.textContent = "Subscribe";
+        localStorage.setItem("savedEmail", inputEmail.value);
+        textApproved.style.display = "block";
+        textApproved.textContent = "Successfully accomplished.";
+      }, 2000);
+
+      setTimeout(() => {
+        textApproved.style.display = "none";
+        inputEmail.value = "";
+      }, 4500);
+    }
+  } else {
+    iconLoading.style.display = "block";
+    buttonEmail.textContent = "";
+
+    setTimeout(() => {
+      buttonEmail.textContent = "Subscribe";
+      inputEmail.value = "";
+      inputEmail.placeholder = "Invalid email";
+      inputEmail.classList.add("inpuntError");
+      iconLoading.style.display = "none";
+      inputEmail.focus();
+    }, 2000);
+  }
 }
